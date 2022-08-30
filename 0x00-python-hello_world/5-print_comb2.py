af4e336592d3273a78f55c26c27d3cbc8d8fe8dc
CalebHouse99 <calebhouse99@gmail.com>
2022-08-30 09:31:43 -0700

    I think I got it

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 for i in range(00, 100):
+    x = '00'
     if i < 10:
-        print(i, end=", ")
+        print("{}{}".format(0, i), end=", ")
     elif i == 99:
-        print(i, end="")
+        print("{}".format(i), end="")
     else:
-        print(i, end=", ")
+        print("{}".format(i), end=", ")
