256c27d8edb1204da6ac7d402c7342452b3504f3
CalebHouse99 <calebhouse99@gmail.com>
2022-08-30 09:04:02 -0700

    python simply rocks

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = number % 10
+if number < 0 and digit != 0:
+    digit = 10 - digit
+if digit > 5:
+    last_message = "and is greater than 5"
+if digit < 6:
+    last_message = "and is less than 6 and not 0"
+if digit == 0:
+    last_message = "and is 0"
+print("The last digit of", number, "is", digit, last_message)
