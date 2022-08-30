534b79345d4aa6bd367dc4c30ac84542810ef47c
CalebHouse99 <calebhouse99@gmail.com>
2022-08-30 09:45:09 -0700

    checker time

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(00, 100):
-    x = '00'
-    if i < 10:
-        print("{}{}".format(0, i), end=", ")
     elif i == 99:
         print("{}".format(i), end="")
     else:
