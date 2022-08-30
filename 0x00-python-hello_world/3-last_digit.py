0215293f95600ac9761784957b3deb7f3838f097
CalebHouse99 <calebhouse99@gmail.com>
2022-08-30 09:07:49 -0700

    the last digit of -98 is -8 ok

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 digit = number % 10
 if number < 0 and digit != 0:
-    digit = 10 - digit
+    digit = (10 - digit) * -1
 if digit > 5:
     last_message = "and is greater than 5"
 if digit < 6:
