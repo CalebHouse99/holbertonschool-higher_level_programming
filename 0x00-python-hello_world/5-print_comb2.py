5d6f7f8768a9d9a444e763dfc610abd9e27cf2ea
CalebHouse99 <calebhouse99@gmail.com>
2022-08-30 11:14:51 -0700

    I GOT IT!!!

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-for i in range(00, 100):
+for i in range(0, 100):
+    tens = (i % 100) // 10
+    ones = i % 10
     if i == 99:
         print("{}".format(i), end="")
     else:
-        print("{}".format(i), end=", ")
+        print("{}{}".format(tens, ones), end=", ")
