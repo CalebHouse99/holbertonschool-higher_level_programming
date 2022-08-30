54cfb1f0e2588bda3fd2f95babb3546f02a3b0c5
CalebHouse99 <calebhouse99@gmail.com>
2022-08-30 13:15:05 -0700

    I think i did too much work. but it works

---
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+i = 0
+
+while i < 90:
+    tens = (i % 100) // 10
+    ones = i % 10
+    ones_savepoint = tens + 1
+    if tens > 0 and ones == 0:
+        ones = ones_savepoint
+        i = (tens * 10) + ones
+        print("{}{}".format(tens, ones), end=", ")
+        i+=1
+        continue
+    elif i == 89:
+        print("{}".format(i), end="\n")
+        i+=1
+    else: 
+        print("{}{}".format(tens, ones), end=", ")
+        i+=1
