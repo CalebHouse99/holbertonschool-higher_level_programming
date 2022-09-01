fb9be539905813f05e96b789119907272c28db42
CalebHouse99 <calebhouse99@gmail.com>
2022-09-01 15:55:29 -0700

    edge case

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) = 0:
+        return None
     tuple_a = [len(sentence), sentence[0]]
     tuple_a = tuple(tuple_a)
     return tuple_a
