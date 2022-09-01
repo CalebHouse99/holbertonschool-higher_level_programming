3b914568b13fbaea85c0133c6fd16d25e9435ce2
CalebHouse99 <calebhouse99@gmail.com>
2022-08-31 17:37:55 -0700

    fixed range

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
 if sys.argv == None:
     print("{}.".format(len(sys.argv)))
 else:
-    print("{} arguments:{} {}".format(len(sys.argv), '\n', sys.argv[]))
+    print("{} arguments:{} {}".format(len(sys.argv), '\n', sys.argv[1:]))
