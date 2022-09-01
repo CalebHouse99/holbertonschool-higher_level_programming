2dfbed859044bc47e52c97db3572cb154ac9caaa
CalebHouse99 <calebhouse99@gmail.com>
2022-08-31 19:41:33 -0700

    got it

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -4,7 +4,10 @@
 def safe_print_division(a, b):
     try:
         result = a // b
-        print("{}".format(result))
+        print("Inside result: {}".format(float(result)))
         return result
     except ZeroDivisionError:
+        print("Inside result: {}".format("None"))
         return None
+    finally:
+        pass
