6102d4b1906a4d635700775e64c52e836d5ac383
CalebHouse99 <calebhouse99@gmail.com>
2022-09-01 15:45:05 -0700

    brutal tuples

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    newple = []
     len_a = len(tuple_a)
     len_b = len(tuple_b)
-    for i in range(0, len(tuple_a)):
-        if len_a < 2:
+    if len_a < 2:
+        while len_a < 2:
             tuple_a += (0,)
             len_a += 1
-        if len_b < 2:
+    if len_b < 2:
+        while len_b < 2:
             tuple_b += (0,)
             len_b += 1
-        newple.append(tuple_a[i]+tuple_b[i])
-    newple = tuple(newple)
+    newple = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return newple
