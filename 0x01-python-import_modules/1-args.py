860571fb7556de40b1cf303d64ea4ebb634740dd
CalebHouse99 <calebhouse99@gmail.com>
2022-08-31 17:46:06 -0700

    hello case

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
 if len(sys.argv) - 1 == 0:
     print("{}.".format(len(sys.argv)))
 elif len(sys.argv) - 1 == 1:
-    print("{} argument:{}1: {}".format(len(sys.argv) - 1, '\n', sys.argv[1:]))
+    print("{} argument:{}1: {}".format(len(sys.argv) - 1, '\n', sys.argv[1]))
 else:
-    print("{} arguments:{} {}".format(len(sys.argv) - 1, '\n', sys.argv[1:]))
+    print("{} arguments:{}{}:{}".format(len(sys.argv) - 1, '\n', format(len(sys.argv) - 1, sys.argv[1:]))
