e02505cd703cd36163435571c7b0cffd8179bb7f
CalebHouse99 <calebhouse99@gmail.com>
2022-08-31 18:22:58 -0700

    for

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -7,12 +7,14 @@ def main():
 if __name__ == "__main__":
     main()
 
+n = len(sys.argv)
+
 if len(sys.argv) - 1 == 0:
     print("{}.".format(len(sys.argv)))
 elif len(sys.argv) - 1 == 1:
     print("{} argument:{}1: {}".format(len(sys.argv) - 1, '\n', sys.argv[1]))
 else:
     print("{} arguments:".format(len(sys.argv) - 1), end="")
-    for i in sys.argv:
+    for i in range(1, n):
         if i < len(sys.argv) - 1:
             print("{}{}:{}".format(len(sys.argv) - 1, '\n', len(sys.argv) - 1, sys.argv[i]))
