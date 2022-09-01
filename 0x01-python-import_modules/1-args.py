376849812ba8ce825051daac7d0f6bbd29887a63
CalebHouse99 <calebhouse99@gmail.com>
2022-08-31 17:40:48 -0700

    major fixes

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -7,7 +7,9 @@ def main():
 if __name__ == "__main__":
     main()
 
-if sys.argv == None:
+if len(sys.argv) - 1 == 0:
     print("{}.".format(len(sys.argv)))
+elif len(sys.argv) - 1 == 1:
+    print("{} argument:{} {}".format(len(sys.argv) - 1, '\n', sys.argv[1:]))
 else:
-    print("{} arguments:{} {}".format(len(sys.argv), '\n', sys.argv[1:]))
+    print("{} arguments:{} {}".format(len(sys.argv) - 1, '\n', sys.argv[1:]))
