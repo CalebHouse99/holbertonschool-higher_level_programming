2aaf85643fb3bf450c0f0e5ee283d0f1d89e9dc2
CalebHouse99 <calebhouse99@gmail.com>
2022-08-31 17:29:39 -0700

    shot in the dark

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+if sys.argv == None:
+    print("{}.", len(sys.argv))
+else:
+    print("{}: {}"len(sys.argv))
