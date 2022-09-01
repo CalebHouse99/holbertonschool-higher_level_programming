b0a82f1f2fb59f2b348b613b25628668163672a9
CalebHouse99 <calebhouse99@gmail.com>
2022-09-01 15:57:35 -0700

    successful edge case

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        edge_case = [0, None]
+        edge_case = tuple(edge_case)
+        return edge_case
     tuple_a = [len(sentence), sentence[0]]
     tuple_a = tuple(tuple_a)
     return tuple_a
