70b3799dbe6295c10a027ecdccf108bd2d5b5237
CalebHouse99 <calebhouse99@gmail.com>
2022-09-01 12:45:37 -0700

    works locally

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    print('{}'.format('\n').join(['{}'.format(" ").join([str(cell) for cell in row]) for row in matrix]))
+    for row in matrix:
+        for i in range(0, len(row)):
+            print("{:d}".format(row[i]), end=" ")
+        print("{}".format(''))
