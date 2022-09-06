c6a43172cb3bc1a39671172a1c60978de0689e53
CalebHouse99 <calebhouse99@gmail.com>
2022-09-06 11:56:36 -0700

    forgot to pep8

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,3 @@ def print_sorted_dictionary(a_dictionary):
     a_dictionary = dict(sorted(a_dictionary.items()))
     for key in a_dictionary:
         print("{}: {}".format(key, a_dictionary[key]))
-        
