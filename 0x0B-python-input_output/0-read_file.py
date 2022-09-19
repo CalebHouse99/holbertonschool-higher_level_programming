2036a2c704b0de3459fc8615627915989169a98c
CalebHouse99 <calebhouse99@gmail.com>
2022-09-19 15:59:30 -0700

    comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Reads text and outputs it
+"""
+
+
 def read_file(filename=""):
+    """Reads text and outputs it"""
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content)
