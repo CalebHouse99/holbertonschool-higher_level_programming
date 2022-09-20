2eed65c948dac729381b2d9da47cd92983a252cd
CalebHouse99 <calebhouse99@gmail.com>
2022-09-19 21:54:07 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Student"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """student attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """class to json"""
+        if attrs is None:
+            return (vars(self))
+        attributes = {}
+        for key, value in vars(self).items():
+            if key in attrs:
+                attributes[key] = value
+        return (attributes)
+
+    def reload_from_json(self, json):
+        """data in place of attributes"""
+
+        for key, value in json.items():
+            setattr(self, key, value)
