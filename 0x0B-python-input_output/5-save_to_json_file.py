f480bb05a7220962dcb8b15941fb0cd7011d7781
CalebHouse99 <calebhouse99@gmail.com>
2022-09-19 20:58:07 -0700

    cut out the variable

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write to text file from object"""
-    jason_string = json.dumps(my_obj)
     with open(filename, "w", encoding="utf-8") as file:
-        file.write(jason_string)
+        file.write(json.dumps(my_obj))
