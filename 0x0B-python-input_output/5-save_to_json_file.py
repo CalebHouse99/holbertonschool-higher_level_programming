eed6e607cf5182c5da130e1cf82ee89bace42815
CalebHouse99 <calebhouse99@gmail.com>
2022-09-19 20:47:54 -0700

    one output check

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,4 +9,3 @@ def save_to_json_file(my_obj, filename):
     jason_string = json.dumps(my_obj)
     with open(filename, "w", encoding="utf-8") as file:
         file.write(jason_string)
-        file.close
