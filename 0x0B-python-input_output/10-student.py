5e2e98c4fd98790df5959b4a2a7f6d008200348d
CalebHouse99 <calebhouse99@gmail.com>
2022-09-19 21:44:47 -0700

    task 10 init commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Student"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """student attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        self.attrs = attrs
+        at_list = {}
+        if attrs == None:
+            return self.__dict__
+        for i in attrs:
+            if i in self.__dict__:
+                at_list[i] = self.__dict__[i]
+        return at_list
