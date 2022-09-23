a5d1e03c88ec54b40558f0d7851da55085a55a76
CalebHouse99 <calebhouse99@gmail.com>
2022-09-23 12:08:09 -0700

    added super id

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(Base):
     """Rectangle Base Class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialization of the self"""
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
