04dff0315d81c9e0b1085a3c8b298d27df955210
CalebHouse99 <calebhouse99@gmail.com>
2022-09-23 10:05:24 -0700

    additions

---
+++ 0x0C-python-almost_a_circle/models/README.md
@@ -0,0 +1,1 @@
+These our the models
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 class Base:
     """Almost a Circle"""
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
