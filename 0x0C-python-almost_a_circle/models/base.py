67f6bf5d58a7eeb1ca27c20bccabb7d91769ed4a
CalebHouse99 <calebhouse99@gmail.com>
2022-09-23 21:17:56 -0700

    task 17 init commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,10 @@ class Base:
             else:
                 list_dictionaries = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list from json string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
