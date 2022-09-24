318c90dd1cb7762adaa4141edbabbe454a6dac12
CalebHouse99 <calebhouse99@gmail.com>
2022-09-23 21:24:57 -0700

    instance with atts

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,14 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns instance with atts set"""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                pumpkin = cls(2693877, 21736)
+            else:
+                pumpkin = cls(6)
+            pumpkin.update(**dictionary)
+            return pumpkin
