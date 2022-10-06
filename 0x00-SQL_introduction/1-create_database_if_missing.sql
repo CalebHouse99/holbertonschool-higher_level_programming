97de3935b2f2d416ada866c820c618760ef3ec7c
CalebHouse99 <calebhouse99@gmail.com>
2022-10-05 17:14:28 -0700

    select is a no-no

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,5 @@
 -- The CREATE command creates something, go figure
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbtn_0c_0')
+If(db_id(N'hbtn_0c_0') IS NULL)
 BEGIN
   CREATE DATABASE hbtn_0c_0;
 END;
