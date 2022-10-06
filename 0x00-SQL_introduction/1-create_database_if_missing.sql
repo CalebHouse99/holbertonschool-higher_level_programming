17b045dd1c9ba437a78dc5d3d68c3210b2417b21
CalebHouse99 <calebhouse99@gmail.com>
2022-10-05 17:12:43 -0700

    added if condition

--- 0x00-SQL_introduction/1-create_database_if_missing.sql
+++ 0x00-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,6 @@
 -- The CREATE command creates something, go figure
-CREATE DATABASE hbtn_0c_0;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'hbtn_0c_0')
+BEGIN
+  CREATE DATABASE hbtn_0c_0;
+END;
+GO
