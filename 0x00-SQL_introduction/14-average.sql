4ce1d6ef7a4da3a4f5dcd2f16131877e39c5233b
CalebHouse99 <calebhouse99@gmail.com>
2022-10-05 23:25:14 -0700

    16 init

--- 0x00-SQL_introduction/14-average.sql
+++ 0x00-SQL_introduction/14-average.sql
@@ -1,2 +1,6 @@
 -- average sql comment
-SELECT AVG(score) FROM second_table;
+ALTER TABLE second_table
+ADD average FLOAT;
+INSERT INTO second_table (average)
+VALUES (AVG(score));
+SELECT average FROM second_table;
--- 0x00-SQL_introduction/16-no_link.sql
+++ 0x00-SQL_introduction/16-no_link.sql
@@ -1,1 +1,4 @@
 -- no link
+SELECT score, name FROM second_table 
+WHERE EXISTS (SELECT name from second_table)
+ORDER BY score DESC;
