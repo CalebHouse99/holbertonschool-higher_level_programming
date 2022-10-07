3c8914fe9bd78902ac172d9c43cb102f76506636
CalebHouse99 <calebhouse99@gmail.com>
2022-10-07 12:43:50 -0700

    as average

--- 0x00-SQL_introduction/14-average.sql
+++ 0x00-SQL_introduction/14-average.sql
@@ -1,10 +1,3 @@
 -- average sql comment
-SELECT AVG(score)
+SELECT AVG(score) AS average
 FROM second_table;
-
-
--- ALTER TABLE second_table
--- ADD average FLOAT;
--- INSERT INTO second_table (average)
--- VALUES (AVG(score));
--- SELECT average FROM second_table;
