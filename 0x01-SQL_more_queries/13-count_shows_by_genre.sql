ef85d3465eedbcca8dc4de9274dfb2131b7c9f5a
CalebHouse99 <calebhouse99@gmail.com>
2022-10-10 11:57:06 -0700

    joins are a bit tricky

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from hbtn_0d_tvshows
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres, tv_show_genres
-WHERE tv_show_genres.genre_id = tv_show_genres.show_id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY tv_show_genres.genre_id DESC;
+ORDER BY number_of_shows DESC;
