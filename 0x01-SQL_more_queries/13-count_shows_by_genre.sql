0b34baf3e5c45851af2b76fd06b5662bf558fc18
CalebHouse99 <calebhouse99@gmail.com>
2022-10-10 11:38:57 -0700

    on where

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,5 @@
 -- lists all genres from hbtn_0d_tvshows
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres, tv_show_genres
+WHERE tv_show_genres.genre_id = tv_show_genres.show_id
+GROUP BY tv_genres.name
+ORDER BY tv_show_genres.genre_id DESC;
