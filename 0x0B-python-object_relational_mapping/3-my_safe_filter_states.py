d1bad378bbe4accd82254ea234124a57a3867760
CalebHouse99 <calebhouse99@gmail.com>
2022-10-20 10:54:08 -0700

    all args use c syntax

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,8 +6,10 @@ import sys
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user="%s" % sys.argv[1],
+                           passwd="%s" % sys.argv[2],
+                           db="%s" % sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE \
         states.name LIKE BINARY '%s' ORDER BY id ASC;" % sys.argv[4])
