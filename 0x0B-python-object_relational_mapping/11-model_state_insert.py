4607b08ea27aba5af771da2be20a42e2c539715e
CalebHouse99 <calebhouse99@gmail.com>
2022-10-21 12:24:05 -0700

    no id

--- 0x0B-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -14,12 +14,11 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    print(states.id)
     session = Session(engine)
 
-    louie = State(id=6, name="Louisiana")
+    louie = State(name="Louisiana")
 
     session.add(louie)
     session.commit()
-    print(states.id)
+    print(louie.id)
     session.close()
