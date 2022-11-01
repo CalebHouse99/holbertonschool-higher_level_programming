8b0445c4f1fe558beff9afd514d9cc79c0a86323
CalebHouse99 <calebhouse99@gmail.com>
2022-11-01 12:32:34 -0700

    thanks paul

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         print("Body response:")
         response = response.read()
-        print("    - type: {}".format(str(type(response))))
-        print("    - content: {}".format(str(response)))
-        print("    - utf8 content: {}".format(response.decode('utf-8')))
+        print("\t- type: {}".format(str(type(response))))
+        print("\t- content: {}".format(str(response)))
+        print("\t- utf8 content: {}".format(response.decode('utf-8')))
