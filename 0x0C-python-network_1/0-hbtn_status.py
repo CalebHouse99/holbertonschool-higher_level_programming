e03c8738d6c67228ea73fa9307ccba74e26b0d55
CalebHouse99 <calebhouse99@gmail.com>
2022-11-01 12:31:22 -0700

    name = main

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """First web scrape"""
-from urllib import request
+if __name__ == "__main__":
+    from urllib import request
 
-with request.urlopen('https://intranet.hbtn.io/status') as response:
-    print("Body response:")
-    response = response.read()
-    print("    - type: {}".format(str(type(response))))
-    print("    - content: {}".format(str(response)))
-    print("    - utf8 content: {}".format(response.decode('utf-8')))
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        print("Body response:")
+        response = response.read()
+        print("    - type: {}".format(str(type(response))))
+        print("    - content: {}".format(str(response)))
+        print("    - utf8 content: {}".format(response.decode('utf-8')))
