97f8f438eab097a3d76adbe04a04ba58d0138c0a
CalebHouse99 <calebhouse99@gmail.com>
2022-11-01 19:08:45 -0700

    pycodestyle

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     from sys import argv
     from urllib import request
     from urllib.parse import urlparse, parse_qs
-    
+
     with request.urlopen(argv[1]) as response:
         print(response.getheader('X-Request-Id'))
