3eea4d08e87c05000ade661b411edce3639ccd6a
CalebHouse99 <calebhouse99@gmail.com>
2022-11-04 07:51:11 -0700

    r

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
 
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
