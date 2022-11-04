b43841fdf813f13e902c6a0af81cdc82449f6ad1
CalebHouse99 <calebhouse99@gmail.com>
2022-11-04 07:40:41 -0700

    post request

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -3,11 +3,10 @@
 if __name__ == "__main__":
     from sys import argv
     from urllib import request, parse
-    import urllib
 
-    values = {}
-    print('open is assigned to %r' % open)
-    with request.Request(argv[1], method="POST") as req:
-        r = request.urlencode(values)
-        req = urllib.request.Request(argv[1], values)
-        print(req)
+    url = sys.argv[1]
+    value = {'email': argv[2]}
+    data = parse.urlencode(value).encode('utf-8')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
