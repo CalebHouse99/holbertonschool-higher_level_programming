717e0a4b5644b3c5c1f61a50ea00731bf8d3bcb8
CalebHouse99 <calebhouse99@gmail.com>
2022-11-06 21:29:50 -0800

    changes from pld

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -9,4 +9,5 @@ if __name__ == "__main__":
     url = ""
     username = argv[1]
     token = argv[2]
-    req = requests.get(url, auth=(username, toaken)).json()
+    req = requests.get(url, auth=HTTPBasicAuth(username, token)).json()
+    print(req.get('id'))
--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -20,3 +20,5 @@ if __name__ == "__main__":
             print("no result")
         else:
             print("")
+    except Exception as e:
+        print("Not a valid JSON")
