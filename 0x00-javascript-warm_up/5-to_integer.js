0807647402d1c346ce0e70c413843338872f4acd
CalebHouse99 <calebhouse99@gmail.com>
2022-12-05 19:51:02 -0800

    first 6

---
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const int = parseInt(process.argv[2]);
+if (isNaN(input)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + int);
+}
---
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const statements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+for (i = 0; i < statements.length; i++) {
+  console.log(statements[i]);
+}
