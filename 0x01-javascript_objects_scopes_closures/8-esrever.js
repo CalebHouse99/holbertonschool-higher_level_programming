cdd17c0a00c95797810d7e29a80386f8026a272d
CalebHouse99 <calebhouse99@gmail.com>
2022-12-05 21:42:17 -0800

    semistandard

--- 0x01-javascript_objects_scopes_closures/8-esrever.js
+++ 0x01-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const reverseIt = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-      reverseIt.push(list[i]);
-    }
-    return (reverseIt);
-  };
+  const reverseIt = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverseIt.push(list[i]);
+  }
+  return (reverseIt);
+};
