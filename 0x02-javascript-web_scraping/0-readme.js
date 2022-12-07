d3df0e5f336e50925fcc5995fc5d62f805cf72f4
CalebHouse99 <calebhouse99@gmail.com>
2022-12-06 18:22:03 -0800

    const, and ternary is where its at

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // File System Object
-var fs = require("fs");
+const fs = require("fs");
 // Read File
 fs.readFile(process.argv[2], "utf8", function (err, data) {
   err ? Function("error", "throw error")(err) : console.log(data);
