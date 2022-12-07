ddee06b80a444c06bebafd47132e559a4893e385
CalebHouse99 <calebhouse99@gmail.com>
2022-12-07 08:21:12 -0800

    task 4

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body).results;
+    let i = 0;
+    for (const title in data) {
+      const chars = data[title].characters;
+      for (const char in chars) {
+        if (chars[char].includes('/18/')) {
+          i++;
+        }
+      }
+    }
+    console.log(i);
+  }
+});
