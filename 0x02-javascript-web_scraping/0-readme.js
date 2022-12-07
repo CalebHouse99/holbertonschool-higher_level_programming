6edfb8b08ea0bc1a1f6c6361e1c4e122d7cb7718
CalebHouse99 <calebhouse99@gmail.com>
2022-12-06 18:24:03 -0800

    semistandard

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // File System Object
-const fs = require("fs");
+const fs = require('fs');
 // Read File
-fs.readFile(process.argv[2], "utf8", function (err, data) {
-  err ? Function("error", "throw error")(err) : console.log(data);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  err ? Function('error', 'throw error')(err) : console.log(data);
 });
