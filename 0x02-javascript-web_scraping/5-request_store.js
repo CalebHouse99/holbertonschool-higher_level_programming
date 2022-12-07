795ef399c57fa46efc8166ce9213cdd9e00fba5d
CalebHouse99 <calebhouse99@gmail.com>
2022-12-06 21:34:52 -0800

    task 5

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) throw error;
+  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+    if (err) throw err;
+  });
+});
