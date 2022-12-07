51782493952a4a05c314a23fca1c48dabf85c492
CalebHouse99 <calebhouse99@gmail.com>
2022-12-07 08:25:51 -0800

    task 6

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  const tasks = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      if (tasks[task.userId]) {
+        tasks[task.userId]++;
+      } else {
+        tasks[task.userId] = 1;
+      }
+    }
+  }
+  console.log(tasks);
+});
