6e5a44f19c01dab1668177912ecfe843ef07ca87
CalebHouse99 <78427735+CalebHouse99@users.noreply.github.com>
2022-12-08 19:13:45 -0600

    Updated Link, that was the only edit today

--- 0x03-javascript-web_jquery/9-script.js
+++ 0x03-javascript-web_jquery/9-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', (content) => {
+  $.get('https://stefanbohacek.com/hellosalut/?lang=fr', (content) => {
     $('#hello').text(content.hello);
   });
 });
